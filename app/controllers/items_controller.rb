1314ef0aa2f8313f9dc1ba125958775cd4b89b2a
Gengen27 <tachibana7227@gmail.com>
2025-09-29 16:39:34 +0900

    商品出品機能-2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,36 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
+    @items = Item.all.order(created_at: :desc)
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to root_path, notice: "出品が完了しました"
+    else
+      flash.now[:alert] = "入力内容に問題があります"
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :category_id, :condition_id,
+      :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id,
+      :price, :image
+    )
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,26 +1,35 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one :purchase
   has_one_attached :image
+  has_one :purchase
 
-  # ActiveHash 関連
+  # ActiveHashとの関連付け
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category
-  belongs_to_active_hash :condition
-  belongs_to_active_hash :shipping_fee
-  belongs_to_active_hash :prefecture
-  belongs_to_active_hash :shipping_day
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
 
   # バリデーション
-  validates :name, :description, :category_id, :condition_id,
-            :shipping_fee_id, :prefecture_id, :shipping_day_id,
-            :price, :image, presence: true
+  with_options presence: true do
+    validates :image
+    validates :name, length: { maximum: 40 }
+    validates :description, length: { maximum: 1000 }
+    validates :price, numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 300,
+      less_than_or_equal_to: 9_999_999
+    }
+  end
 
-  validates :category_id, :condition_id, :shipping_fee_id,
-            :prefecture_id, :shipping_day_id,
-            numericality: { other_than: 0, message: "を選択してください" }
-
-  validates :price, numericality: { only_integer: true,
-                                    greater_than_or_equal_to: 300,
-                                    less_than_or_equal_to: 9_999_999 }
+  # ActiveHashの選択肢で「---」(id=1) を弾く
+  with_options numericality: { other_than: 1, message: "を入力してください" } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
 end
+
