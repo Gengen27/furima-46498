0f1f1a9e7fe3ae402cf019bdc03e81d2e8eb0f81
Gengen27 <tachibana7227@gmail.com>
2025-10-01 20:26:19 +0900

    商品削除機能-2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,7 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    redirect_to root_path, notice: "商品を削除しました"
+    redirect_to root_path, notice: '商品を削除しました'
   end
 
   private
