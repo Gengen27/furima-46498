632023790308c49fcfd7148598b6b23d63a03368
Gengen27 <tachibana7227@gmail.com>
2025-10-04 17:59:44 +0900

    デプロイ

--- db/migrate/20250926024540_add_not_null_to_users.rb
+++ db/migrate/20250926024540_add_not_null_to_users.rb
@@ -1,27 +1,44 @@
 class AddNotNullToUsers < ActiveRecord::Migration[7.1]
   def up
-    User.where(nickname: nil).update_all(nickname: "guest")
-    User.where(last_name: nil).update_all(last_name: "guest")
-    User.where(first_name: nil).update_all(first_name: "guest")
-    User.where(last_name_kana: nil).update_all(last_name_kana: "ゲスト")
-    User.where(first_name_kana: nil).update_all(first_name_kana: "ゲスト")
-    User.where(birthday: nil).update_all(birthday: Date.today)
-
-    change_column_null :users, :nickname, false
-    change_column_null :users, :last_name, false
-    change_column_null :users, :first_name, false
-    change_column_null :users, :last_name_kana, false
-    change_column_null :users, :first_name_kana, false
-    change_column_null :users, :birthday, false
+    # カラムが存在するかをチェックしてから処理を実行
+    if column_exists?(:users, :nickname)
+      User.where(nickname: nil).update_all(nickname: "guest")
+      change_column_null :users, :nickname, false
+    end
+    
+    if column_exists?(:users, :last_name)
+      User.where(last_name: nil).update_all(last_name: "guest")
+      change_column_null :users, :last_name, false
+    end
+    
+    if column_exists?(:users, :first_name)
+      User.where(first_name: nil).update_all(first_name: "guest")
+      change_column_null :users, :first_name, false
+    end
+    
+    if column_exists?(:users, :last_name_kana)
+      User.where(last_name_kana: nil).update_all(last_name_kana: "ゲスト")
+      change_column_null :users, :last_name_kana, false
+    end
+    
+    if column_exists?(:users, :first_name_kana)
+      User.where(first_name_kana: nil).update_all(first_name_kana: "ゲスト")
+      change_column_null :users, :first_name_kana, false
+    end
+    
+    if column_exists?(:users, :birthday)
+      User.where(birthday: nil).update_all(birthday: Date.today)
+      change_column_null :users, :birthday, false
+    end
   end
 
   def down
-    change_column_null :users, :nickname, true
-    change_column_null :users, :last_name, true
-    change_column_null :users, :first_name, true
-    change_column_null :users, :last_name_kana, true
-    change_column_null :users, :first_name_kana, true
-    change_column_null :users, :birthday, true
+    change_column_null :users, :nickname, true if column_exists?(:users, :nickname)
+    change_column_null :users, :last_name, true if column_exists?(:users, :last_name)
+    change_column_null :users, :first_name, true if column_exists?(:users, :first_name)
+    change_column_null :users, :last_name_kana, true if column_exists?(:users, :last_name_kana)
+    change_column_null :users, :first_name_kana, true if column_exists?(:users, :first_name_kana)
+    change_column_null :users, :birthday, true if column_exists?(:users, :birthday)
   end
 end
 
