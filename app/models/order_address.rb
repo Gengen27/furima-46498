6dd834c2b03ace37efea97275aad4bba5ce50e8e
Gengen27 <tachibana7227@gmail.com>
2025-10-07 15:54:09 +0900

    エラー文

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,7 +10,7 @@ class OrderAddress
   validates :user_id, :item_id, :city, :house_number, :token, presence: true
   validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :phone_number, presence: true, length: { minimum: 10, message: "is too short" }
+  validates :phone_number, presence: true
   validate :phone_number_format
   
   # building_nameは任意項目（バリデーションなし）
@@ -18,7 +18,17 @@ class OrderAddress
   # カスタムバリデーション
   def phone_number_format
     return if phone_number.blank?
-    unless phone_number.match?(/\A\d{10,11}\z/)
+    
+    # 数字以外が含まれている場合
+    unless phone_number.match?(/\A\d+\z/)
+      errors.add(:phone_number, "is invalid. Input only number")
+      return
+    end
+    
+    # 桁数チェック
+    if phone_number.length < 10
+      errors.add(:phone_number, "is too short")
+    elsif phone_number.length > 11
       errors.add(:phone_number, "is invalid. Input only number")
     end
   end
